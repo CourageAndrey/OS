0f480cb6dbda7da52d679189679c8fb6a709efaa
Andrei Bychko <andrei.a.bychko@gmail.com>
2022-05-27 11:12:36 +0300

    Add arrays and digits.

---
+++ VirtualMachine/VirtualMachine/Core/Array.cs
@@ -0,0 +1,44 @@
+﻿using VirtualMachine.Reflection;
+
+namespace VirtualMachine.Core
+{
+	public class Array : Object
+	{
+		#region Properties
+
+		public Integer Length
+		{
+			get { return (Integer) _data[ObjectDataType.Fields.Count + 0]; }
+			internal set { _data[ObjectDataType.Fields.Count + 0] = value; }
+		}
+
+		internal readonly Object[] Items;
+
+		#endregion
+
+		#region Conctructors
+
+		public Array(Integer length)
+		{
+			Items = new Object[length.Value];
+		}
+
+		public static Array Create(Integer length)
+		{
+			var result = new Array(length);
+			result.CallConstructor(ArrayDataType);
+			result.Length = length;
+			return result;
+		}
+
+		#endregion
+
+		#region Metadata
+
+		public static readonly DataType ArrayDataType = new DataType();
+
+		internal static DataTypeField FieldLength;
+
+		#endregion
+	}
+}
